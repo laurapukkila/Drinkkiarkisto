05197732fea52c9745744e4bf27aa46562b5ba93
avihavai <avihavai@hp8x-64.cs.helsinki.fi>
2017-11-23 12:34:53 +0200

    githubiin

---
+++ src/main/java/wad/drinkkikirjasto/DrinkkiController.java
@@ -0,0 +1,15 @@
+package wad.drinkkikirjasto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DrinkkiController {
+
+    @GetMapping("*")
+    @ResponseBody
+    public String home() {
+        return "Koti!";
+    }
+}
---
+++ src/main/java/wad/drinkkikirjasto/DrinkkiarkistoApplication.java
@@ -0,0 +1,13 @@
+package wad.drinkkikirjasto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkkiarkistoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkkiarkistoApplication.class);
+    }
+
+}
